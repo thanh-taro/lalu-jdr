9c0a146a423042518f747b0e69ee6c0bf58aabc4
Nguyen Tri Thanh <thanhnt1@greenglobal.vn>
2016-06-06 16:42:53 +0700

    Add functions

--- src/JsonObjects/TopLevel.php
+++ src/JsonObjects/TopLevel.php
@@ -2,6 +2,9 @@
 
 namespace LaLu\JDR\JsonObjects;
 
+use Illuminate\Database\Eloquent\Model;
+use ReflectionClass;
+
 class TopLevel extends Object
 {
     /**
@@ -16,5 +19,29 @@ class TopLevel extends Object
         }
 
         return false;
+    }
+
+    /**
+     * Set eloquent data.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $data
+     *
+     * @return $this
+     */
+    public function setEloquentData(Model $data)
+    {
+        $this->delete('errors');
+        $resource = (new Resource())->set('id', $data->id)
+                        ->set('type', $this->_getTypeFromInstance($data))
+                        ->set('attributes', $data->attributesToArray());
+
+        return $this;
+    }
+
+    private function _getTypeFromInstance($instance)
+    {
+        $reflect = new ReflectionClass($object);
+
+        return strtolower($reflect->getShortName());
     }
 }
