4fa4e5dad4046d1107b16fe00a1ea4ba8878d9ea
Nguyen Tri Thanh <thanhnt1@greenglobal.vn>
2016-06-21 11:06:10 +0700

    Update code

--- src/JsonObjects/V1_0/Error.php
+++ src/JsonObjects/V1_0/Error.php
@@ -15,4 +15,28 @@ class Error extends Object
     {
         return ['id', 'status', 'code', 'title', 'detail', 'source', 'links', 'meta'];
     }
+
+    /**
+     * Convert param into Jsonapi object.
+     *
+     * @param string $field
+     * @param mixed  $params
+     *
+     * @return static
+     */
+    public function convert($field, $params)
+    {
+        switch ($field) {
+            case 'meta':
+                if ($params instanceof Meta) {
+                    return $params;
+                } elseif (is_array($params)) {
+                    return new Meta($params);
+                }
+
+                return;
+            default:
+                return $params;
+        }
+    }
 }
